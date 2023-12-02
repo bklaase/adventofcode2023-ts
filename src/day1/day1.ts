4d0b9035c2187e24333a8b321e7ebb193f36723a
Bob Klaase <>
2023-12-02 09:33:08 +0100

    day1 part2 - still not correct. need to reverse the wordmatching for the last 'digit'

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -2,18 +2,20 @@ import * as fs from 'fs';
 
 let day = 1;
 //let input :string  = fs.readFileSync(`input/day${day}/day${day}-example.txt`, 'utf-8');
-let input :string  = fs.readFileSync(`input/day${day}/day${day}-example2.txt`, 'utf-8');
+// let input :string  = fs.readFileSync(`input/day${day}/day${day}-example2.txt`, 'utf-8');
 
-// let input: string = fs.readFileSync(`input/day${day}/day${day}.txt`, 'utf-8');
+let input: string = fs.readFileSync(`input/day${day}/day${day}.txt`, 'utf-8');
 
 // parse
+// -----
 let parse = (input: string): string[] => {
   let output = input.split('\n');
   output.pop();
   return output;
 }
 
-// solutions
+// solve
+// -----
 interface lineValues { firstDigit: number, lastDigit: number };
 
 let getDigitsInLine = (line: string): lineValues => {
@@ -33,33 +35,34 @@ let compLineValue = (line: string): number => {
   return Number("" + firstDigit + lastDigit);
 }
 
-interface wordDigit {digit: string, word: string}
 let replaceWordsWithDigits = (line: string): string => {
-  let digitMappings: wordDigit[] = [
-    {digit: "1", word: "one"},
-    {digit: "2", word: "two"},
-    {digit: "3", word: "three"},
-    {digit: "4", word: "four"},
-    {digit: "5", word: "five"},
-    {digit: "6", word: "six"},
-    {digit: "7", word: "seven"},
-    {digit: "8", word: "eight"},
-    {digit: "9", word: "nine"}];
+  let digitMappings: {[word: string]: string} =
+      {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+       "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
-  return digitMappings.reduce((line: string, combi: wordDigit) => 
-    line.replaceAll(combi.word, combi.digit), line);
+  let wordrgx = /one|two|three|four|five|six|seven|eight|nine/
+  let match: RegExpMatchArray | null = line.match(wordrgx);
+  while(match) {
+    const matchedWord = match?.[0];
+    line = line.replace(matchedWord, digitMappings[matchedWord])
+    
+    match = line.match(wordrgx);
+  }
+  return line
 }
 
-// part1
+// Assemble parts
+// --------------
 let part1 = (input: string[]): number =>
     input.map(compLineValue)
     .reduce((a, b) => a + b);
 
-let part2 = (input: string[]): number => {
-  return part1(input.map(replaceWordsWithDigits))
-};
+let part2 = (input: string[]): number => 
+ part1(input.map(replaceWordsWithDigits))
 
-// results
+
+// print results
+// -------------
 let parsed = parse(input);
 console.log(`Part1: ${part1(parsed)}`);
 console.log(`Part2: ${part2(parsed)}`);
